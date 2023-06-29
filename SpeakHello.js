696d96207724db5545d0f64963058a1e26d9d775
henchmotion <119769018+henchmotion@users.noreply.github.com>
2023-06-30 00:13:58 +0100

    Create SpeakHello.js

---
+++ SpeakHello.js
@@ -0,0 +1,19 @@
+(function (window) {
+    var helloSpeaker = {};
+    helloSpeaker.speakWord = "Hello";
+    helloSpeaker.speak = function (name) {
+      console.log(helloSpeaker.speakWord + " " + name);
+    }
+    
+
+    window.helloSpeaker = helloSpeaker;
+
+})(window);
+
+
+
+
+
+
+
+
